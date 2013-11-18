a316f18145d4bbdd64ab7d65badc6add7ca21f17
Kazuto AGENA <ie.e115703@gmail.com>
2013-11-18 22:15:46 +0900

    EclipseUpdate,Kanpe03の完成

---
+++ src/kitaken_kadai/kanpe_java03/Kanpe_java03.java
@@ -0,0 +1,11 @@
+package kitaken_kadai.kanpe_java03;
+
+public class Kanpe_java03 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!!");
+		System.out.println("Hello, World!!");
+		System.out.println("Hello, World!!");
+	}
+
+}
