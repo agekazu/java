1b268362f458a2e44e4f2845f7e1f9b6d858625f
Kazuto AGENA <ie.e115703@gmail.com>
2013-12-01 14:31:35 +0900

    Push kanpe_java07

---
+++ src/kitaken_kadai/kanpe_java07/Kanpe_java07.java
@@ -0,0 +1,15 @@
+package kitaken_kadai.kanpe_java07;
+
+public class Kanpe_java07 {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		
+		if(a < 10){
+			System.out.println("a < 10です");
+		}else{
+			System.out.println("10 <= aです");
+		}
+	}
+}
