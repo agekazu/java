aa90274e1451c3f909364dafe76b17abd4e78a7c
Kazuto AGENA <ie.e115703@gmail.com>
2013-12-01 15:12:04 +0900

    Push Kanpe_java02 参考: http://www7a.biglobe.ne.jp/~java-master/samples/lang/VersionCheck.html

---
+++ src/kitaken_kadai/kanpe_java02/Kanpe_java02.java
@@ -0,0 +1,10 @@
+package kitaken_kadai.kanpe_java02;
+
+public class Kanpe_java02 {
+
+	public static void main(String[] args) {
+	     System.out.print("JavaVersion: ");
+	     System.out.println(System.getProperty("java.version"));
+	}
+
+}
