3b5e60a2d62e59b3de30cdb44f5f686b5c5bb14c
Kazuto AGENA <ie.e115703@gmail.com>
2013-12-01 15:00:04 +0900

    Push kanpe_java08

---
+++ src/kitaken_kadai/kanpe_java08/Kanpe_java08.java
@@ -0,0 +1,13 @@
+package kitaken_kadai.kanpe_java08;
+
+public class Kanpe_java08 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 100; i++) {
+			if (i % 15 == 0) System.out.println("i="+i+":FizzBuzz");
+			else if (i % 5 == 0)  System.out.println("i="+i+":Buzz"); 
+			else if (i % 3 == 0)  System.out.println("i="+i+":Fizz"); 
+		}
+	}
+}
