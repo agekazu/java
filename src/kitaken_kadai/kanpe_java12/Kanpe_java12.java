d8fda64b9c991c728bfb809fdb6ae2eeadad7891
Kazuto AGENA <ie.e115703@gmail.com>
2013-12-01 16:23:13 +0900

    課題:kanpe12 int getMin(int a, int b, int c)メソッドを書け をPush.

---
+++ src/kitaken_kadai/kanpe_java12/Kanpe_java12.java
@@ -0,0 +1,19 @@
+package kitaken_kadai.kanpe_java12;
+
+public class Kanpe_java12 {
+
+	public static void main(String[] args) {
+		System.out.println(getMin(1,2,3));  //=> 3
+		System.out.println(getMin(3,1,2));  //=> 3
+		System.out.println(getMin(2,3,1));  //=> 3
+		System.out.println(getMin(-1,-1,-2));  //=> -2
+		System.out.println(getMin(1,1,1));  //=> 1
+
+	}
+
+	public static int getMin(int a, int b, int c) {
+		int d = a <= b ? a:b;
+		return d <= c ? d:c;
+	}
+
+}
