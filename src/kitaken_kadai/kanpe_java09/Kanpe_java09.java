c3011967f32df053ba892229adf9d2e78089afd3
Kazuto AGENA <ie.e115703@gmail.com>
2013-12-01 15:31:55 +0900

    Push Kanpe_java09

---
+++ src/kitaken_kadai/kanpe_java09/Kanpe_java09.java
@@ -0,0 +1,19 @@
+package kitaken_kadai.kanpe_java09;
+
+public class Kanpe_java09 {
+
+	public static void main(String[] args) {
+		System.out.println(getMax(6,11));
+		System.out.println(getMax(13,11));
+		System.out.println(getMax(1,1));
+		/* 出力
+		 * 11 
+		 * 13 
+		 * 1 
+		 */
+	}
+
+	private static int getMax(int a, int b){
+		return a >= b ? a : b;
+	}
+}
