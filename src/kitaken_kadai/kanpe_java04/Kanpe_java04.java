a19691f80bdf0cba70522832865107e1635bdb70
Kazuto AGENA <ie.e115703@gmail.com>
2013-12-01 13:43:17 +0900

    push kanpe_java04

---
+++ src/kitaken_kadai/kanpe_java04/Kanpe_java04.java
@@ -0,0 +1,13 @@
+package kitaken_kadai.kanpe_java04;
+
+public class Kanpe_java04 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<3;i++){
+			System.out.println("HelloWorld");
+		}
+
+	}
+
+}
