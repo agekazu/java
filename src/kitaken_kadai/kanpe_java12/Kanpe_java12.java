93a468bed96ee39de9190e243dc683c26be97f30
Kazuto AGENA <ie.e115703@gmail.com>
2013-12-02 20:53:06 +0900

    kanpe13_int getMid(int a, int b, int c)メソッドを書け をPush.

--- src/kitaken_kadai/kanpe_java12/Kanpe_java12.java
+++ src/kitaken_kadai/kanpe_java12/Kanpe_java12.java
@@ -3,17 +3,35 @@ package kitaken_kadai.kanpe_java12;
 public class Kanpe_java12 {
 
 	public static void main(String[] args) {
-		System.out.println(getMin(1,2,3));  //=> 3
-		System.out.println(getMin(3,1,2));  //=> 3
-		System.out.println(getMin(2,3,1));  //=> 3
-		System.out.println(getMin(-1,-1,-2));  //=> -2
-		System.out.println(getMin(1,1,1));  //=> 1
+
+		System.out.println(getMid(1,2,3));  //=> 2
+		System.out.println(getMid(3,1,2));  //=> 2
+		System.out.println(getMid(2,3,1));  //=> 2
+		System.out.println(getMid(-1,-1,-2));  //=> -1
+		System.out.println(getMid(1,1,1));  //=> 1
 
 	}
 
-	public static int getMin(int a, int b, int c) {
-		int d = a <= b ? a:b;
-		return d <= c ? d:c;
+	public static int getMid(int a, int b, int c) {
+
+		if (a <= b){
+			if (b <= c) { 		// a <= b <= c
+				return b;
+			} else if (a <= c){ // a <= c <= b
+			 	return c;
+			} else {			// c <= a <= b
+				return a;
+			}
+		} else {
+			if (a <= c){			// b <= a <= c
+				return a; 
+			} else if(c <= a){		// b <= c <= a
+				return c;
+			} else {				// c <= b <= a
+				return  b;
+			}
+		}
+
 	}
 
 }
