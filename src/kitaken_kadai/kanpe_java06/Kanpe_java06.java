cafb4a779cdfa6cc6a6bf31ef1fc2ce0c43c7b5b
Kazuto AGENA <ie.e115703@gmail.com>
2013-12-15 17:10:24 +0900

    変数名の変更 i=>left j=>right

--- src/kitaken_kadai/kanpe_java06/Kanpe_java06.java
+++ src/kitaken_kadai/kanpe_java06/Kanpe_java06.java
@@ -4,10 +4,10 @@ public class Kanpe_java06 {
 
 	public static void main(String[] args) {
 
-		for(int i=1; i<=9; i++){
-			System.out.println(i+"の段");
-			for(int j=1; j<=9; j++){
-				System.out.println(i+"*"+j+"="+i*j);
+		for(int left=1; left<=9; left++){
+			System.out.println(left+"の段");
+			for(int right=1; right<=9; right++){
+				System.out.println(left+"*"+right+"="+left*right);
 			}
 		}
 	}
