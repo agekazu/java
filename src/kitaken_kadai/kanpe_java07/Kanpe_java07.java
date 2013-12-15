76d05c1fddcd5de2ae0454ef64c7d904f8732cd5
Kazuto AGENA <ie.e115703@gmail.com>
2013-12-15 17:12:04 +0900

    else句を除いた

--- src/kitaken_kadai/kanpe_java07/Kanpe_java07.java
+++ src/kitaken_kadai/kanpe_java07/Kanpe_java07.java
@@ -4,12 +4,13 @@ public class Kanpe_java07 {
 
 	public static void main(String[] args) {
 
-		int a = 10;
-		
+		int a = 1000;
+
 		if(a < 10){
 			System.out.println("a < 10です");
-		}else{
-			System.out.println("10 <= aです");
+			return;
 		}
+
+		System.out.println("10 <= aです");
 	}
 }
