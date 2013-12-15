76e8ed6bc6c5a19871bc5e9d959be2a17551196a
Kazuto AGENA <ie.e115703@gmail.com>
2013-12-15 17:17:00 +0900

    FizzBuzz以外の値も出力するようにした。

--- src/kitaken_kadai/kanpe_java08/Kanpe_java08.java
+++ src/kitaken_kadai/kanpe_java08/Kanpe_java08.java
@@ -5,9 +5,23 @@ public class Kanpe_java08 {
 	public static void main(String[] args) {
 
 		for (int i = 1; i <= 100; i++) {
-			if (i % 15 == 0) System.out.println("i="+i+":FizzBuzz");
-			else if (i % 5 == 0)  System.out.println("i="+i+":Buzz"); 
-			else if (i % 3 == 0)  System.out.println("i="+i+":Fizz"); 
+			if (i % 15 == 0) {
+				System.out.println("i="+i+":FizzBuzz");
+				continue;
+			}
+			else if (i % 5 == 0) {
+				System.out.println("i="+i+":Buzz"); 
+				continue;
+			}
+			else if (i % 3 == 0) {
+				System.out.println("i="+i+":Fizz"); 
+				continue;
+			}
+
+			System.out.println("i="+i);
+
 		}
+
 	}
+
 }
